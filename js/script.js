5957dddd7455fd72847d3fd6abfd777c1baddf6e
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2015-06-27 05:06:12 +0200

    Update
    Release candidate =)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 /*jshint browser: true*/ ;
 
+//////////////////
+// Форма отзыва //
+//////////////////
+
+//Обработка клика по кнопке вызова формы
 var reportLink = document.querySelector(".btn-report");
 var reportPopup = document.querySelector(".popup-report");
 var form = document.querySelector(".report-form");
@@ -7,7 +12,6 @@ var userNameInput = document.querySelector("[name='name']");
 var userEmailInput = document.querySelector("[name='email']");
 var userTextInput = document.querySelector("[name='report-text']");
 
-//Обработка клика по кнопке вызова формы
 reportLink.addEventListener("click", function (event) {
     event.preventDefault();
     reportPopup.classList.add("popup-content-show");
@@ -57,5 +61,93 @@ window.addEventListener("keydown", function (event) {
             reportPopup.classList.remove("popup-content-show");
             reportPopup.classList.remove("popup-error");
         }
+        //Почему бы и не закрывать так же шоппинг попап?
+        if (shoppingPopup.classList.contains("popup-content-show")) {
+            shoppingPopup.classList.remove("popup-content-show");
+        }
+        //И для карты
+        if (bigMap.classList.contains("popup-content-show")) {
+            bigMap.classList.remove("popup-content-show");
+        }
     }
 });
+
+/////////////////////////////////
+// Добавление товара в корзину //
+//             и               //
+// Закрытие попапов крестиком  //
+/////////////////////////////////
+
+var shoppingPopup = document.querySelector(".popup-shopping");
+var checkoutLink = document.querySelector(".btn-checkout");
+var continueShoppingLink = document.querySelector(".btn-continue-shopping");
+
+//Слушаем все клики и фильтруем по классу цели. По хорошему уже тут можно было бы рефракторить весь код и все предыдущие клики запихать в этот обработчик, но пока оставлю так.
+addEventListener("click", function (event) {
+    //Клики по кнопкам купить
+    if (event.target.classList.contains("shop-item-buy-btn")) {
+        event.preventDefault();
+        shoppingPopup.classList.add("popup-content-show");
+    }
+
+    //Клики по красным крестикам
+    if (event.target.classList.contains("popup-close-icon")) {
+        //Форма отзыва
+        if (reportPopup.classList.contains("popup-content-show")) {
+            reportPopup.classList.remove("popup-content-show");
+            reportPopup.classList.remove("popup-error");
+        }
+        //Шоппинг попап
+        if (shoppingPopup.classList.contains("popup-content-show")) {
+            shoppingPopup.classList.remove("popup-content-show");
+        }
+        //Большая карта
+        if (bigMap.classList.contains("popup-content-show")) {
+            bigMap.classList.remove("popup-content-show");
+        }
+    }
+});
+
+checkoutLink.addEventListener("click", function (event) {
+    //Тут должен быть код, связанный с чекаутом, но поскольку это учебный проект, просто закроем окошко, чтобы проверяющий мог спокойно проверять дальше.
+    shoppingPopup.classList.remove("popup-content-show");
+});
+
+continueShoppingLink.addEventListener("click", function (event) {
+    shoppingPopup.classList.remove("popup-content-show");
+});
+
+
+///////////////////
+// Большая карта //
+///////////////////
+
+var bigMap = document.querySelector(".about-big-map-overlay");
+var showBigMapLink = document.querySelector(".show-big-map");
+
+showBigMapLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    bigMap.classList.add("popup-content-show");
+});
+
+//////////////
+// Слайдеры //
+//////////////
+
+//Поскольку слайда всего 2, то тут я считерю
+var slideLeftBtn = document.querySelector(".slide-left");
+var slideRightBtn = document.querySelector(".slide-right");
+var slide1 = document.querySelector(".hero-slider-slide1");
+var slide2 = document.querySelector(".hero-slider-slide2");
+
+var slideFunction = function (event) {
+    event.preventDefault();
+    slide1.classList.toggle("hero-slide-show");
+    slide2.classList.toggle("hero-slide-show");
+};
+
+slideLeftBtn.addEventListener("click", slideFunction);
+
+slideRightBtn.addEventListener("click", slideFunction);
+
+//А вот слайдер для "сервисов" я решил сделать на CSS
